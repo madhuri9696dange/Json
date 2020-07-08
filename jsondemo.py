9c97e5ed82b1e907bbaa5ede0cb5a47b35161ee7
madhuri dange <madhuridange9696@gmail.com>
2020-07-08 16:13:11 +0530

    Third Commit

---
+++ jsondemo.py
@@ -0,0 +1,26 @@
+# Python to json
+
+
+# How to retrieve values from json string in python ......demo -
+
+try:
+    # importing package json
+    import json
+
+# JSON string:
+    json_var = '{ "EmpName":"Tom", "EmpAge":29, "EmpDesignation":"Senior Developer"}'
+
+# parse result :
+    result = json.loads(json_var)
+
+    print(result)
+
+    print(type(result))
+
+    print(f"Employee Name = {result['EmpName']}")
+    print(f"Employee Age = {result['EmpAge']}")
+    print(f"Employee Designation = {result['EmpDesignation']}")
+
+
+except Exception as e:
+    print(f"Error Occurred = {e}")
